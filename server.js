8ea080c3c630c46a51812270ebe24f0cdf89a2ce
udykas <udykas@gmail.com>
2018-06-28 15:59:41 -0400

    one more time

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+server.get('/', (req, res) => {
+    res.status(200).send(`<h2>DB: ${process.env.mongo}</h2>`);
 });
 
 server.use('/api/notes', noteRouter);
